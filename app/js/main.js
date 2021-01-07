b1ebe629d0c69b816cf5fbbac7c42bf3fa537dc9
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-01-08 02:59:19 +0300

    top slider, stock slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,24 @@
 $(function () {
 
+    $('.banner-section__slider').slick({
+        dots: true,
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        autoplaySpeed: 3000,
+        fade:true
 
+    });
 
-
-
+    $('.stock__items').slick({
+        prevArrow: '<button class="stock__btn stock__btn-prev"><img src="../images/left_1.svg" alt=""></button>',
+        nextArrow: '<button class="stock__btn stock__btn-next"><img src="../images/right_1.svg" alt=""></button>',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        autoplaySpeed: 3000,
+    });
 
 
 });
