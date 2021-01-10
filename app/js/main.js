acf7e358729665044096550a8cc1b892df1cd0bd
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-01-11 02:57:29 +0300

    products

--- app/js/main.js
+++ app/js/main.js
@@ -7,28 +7,38 @@ $(function () {
         pauseOnHover: false,
         pauseOnFocus: false,
         autoplaySpeed: 3000,
-        fade:true
+        fade: true
 
     });
 
     $('.stock__items').slick({
         prevArrow: '<button class="stock__btn stock__btn-prev"><img src="../images/left_1.svg" alt=""></button>',
         nextArrow: '<button class="stock__btn stock__btn-next"><img src="../images/right_1.svg" alt=""></button>',
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
-        centerMode: true,
         autoplaySpeed: 3000,
     });
 
-    $('.search__tabs-item').on('click', function(e){
-       e.preventDefault();
+    $('.product-slider').slick({
+        prevArrow: '<button class="product-slider__btn product-slider__btn-prev"><img src="../images/left_1.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__btn product-slider__btn-next"><img src="../images/right_1.svg" alt=""></button>',
+        slidesToShow: 4,
+        slidesToScroll: 1
+    });
 
-       $('.search__tabs-item').removeClass('search__tabs-item--active');
-       $('.search__content-item').removeClass('search__content-item--active');
+    $('.tab').on('click', function (e) {
+        e.preventDefault();
 
-       $(this).addClass('search__tabs-item--active');
-       $($(this).attr('href')).addClass('search__content-item--active');
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
 
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+
+    });
+
+    $('.product-item__heart').on('click', function () {
+        $(this).toggleClass('product-item__heart--active')
     });
 
 
