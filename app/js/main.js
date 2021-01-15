461df074fb0ea42504c20efaefcc45fc30d60279
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-01-15 05:07:51 +0300

    range slider

--- app/js/main.js
+++ app/js/main.js
@@ -55,11 +55,15 @@ $(function () {
     $('.filter-style').styler();
     $('.filter__item-drop').on('click', function () {
         $(this).toggleClass('filter__item-drop--active');
-        $(this).next().slideToggle();
+        $(this).next().slideToggle(100);
     });
 
 
+    $(".js-range-slider").ionRangeSlider();
+
+
+
+
+
 });
-
-
 
